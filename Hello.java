1199da5db337b61a004847aa66d4e54b3eb56308
Himanshi Dafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2025-07-01 12:50:53 +0530

    Added merge sort

--- Hello.java
+++ Hello.java
@@ -11,6 +11,9 @@ class Hello{
     public static void user3(){
         //user3 data - for user3 function
     }
+    public static void merge(){
+        //merge sort
+    }
     public static void main(String[] args) {
         System.err.println("Hello world");
     }
