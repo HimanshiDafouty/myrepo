f7a843c96fc91e4d442d458b7efd8b237c5b24ec
Himanshi Dafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2025-07-01 12:12:14 +0530

    Added user3 function

--- Hello.java
+++ Hello.java
@@ -1,4 +1,7 @@
 class Hello{
+    public static void user3(){
+        //user3 data - for user3 function
+    }
     public static void main(String[] args) {
         System.err.println("Hello world");
     }
