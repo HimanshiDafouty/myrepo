34b64ecd0c79d907e5e8c4d9105552012112f796
Himanshi Dafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2025-07-01 12:00:10 +0530

    Added user1 function

--- Hello.java
+++ Hello.java
@@ -1,4 +1,8 @@
 class Hello{
+
+    public static void user1(){
+        //User1 data for - user1 branch 
+    }
     public static void main(String[] args) {
         System.err.println("Hello world");
     }
