60b72abc1eff12abed69d438c3d79ddd06dcab5e
Himanshi Dafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2025-07-01 12:07:44 +0530

    Added user2

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ class Hello{
     public static void user1(){
         //User1 data for - user1 branch 
     }
+    public static void user2(){
+        //User2 data for -user2 branch 
+    }
     public static void main(String[] args) {
         System.err.println("Hello world");
     }
